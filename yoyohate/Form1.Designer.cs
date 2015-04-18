b727e625568497a60e06a4297df1a123cc4a348a
wsxwang <wsxrafael@126.com>
2015-04-18 12:21:03 +0800

    用途说明
    m*n个方格，随机放置m*n/2个数字（1~m*n/2），老友委托做的，给小孩子练习用的

---
+++ yoyohate/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace yoyohate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_rowcount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_colcount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_gen = new System.Windows.Forms.Button();
+            this.button_export = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(544, 335);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // textBox_rowcount
+            // 
+            this.textBox_rowcount.Location = new System.Drawing.Point(26, 6);
+            this.textBox_rowcount.Name = "textBox_rowcount";
+            this.textBox_rowcount.Size = new System.Drawing.Size(100, 21);
+            this.textBox_rowcount.TabIndex = 1;
+            this.textBox_rowcount.Text = "12";
+            this.textBox_rowcount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_rowcount_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "行";
+            // 
+            // textBox_colcount
+            // 
+            this.textBox_colcount.Location = new System.Drawing.Point(165, 6);
+            this.textBox_colcount.Name = "textBox_colcount";
+            this.textBox_colcount.Size = new System.Drawing.Size(100, 21);
+            this.textBox_colcount.TabIndex = 1;
+            this.textBox_colcount.Text = "10";
+            this.textBox_colcount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_colcount_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "列";
+            // 
+            // button_gen
+            // 
+            this.button_gen.Location = new System.Drawing.Point(276, 5);
+            this.button_gen.Name = "button_gen";
+            this.button_gen.Size = new System.Drawing.Size(75, 23);
+            this.button_gen.TabIndex = 3;
+            this.button_gen.Text = "生成";
+            this.button_gen.UseVisualStyleBackColor = true;
+            this.button_gen.Click += new System.EventHandler(this.button_gen_Click);
+            // 
+            // button_export
+            // 
+            this.button_export.Location = new System.Drawing.Point(362, 5);
+            this.button_export.Name = "button_export";
+            this.button_export.Size = new System.Drawing.Size(75, 23);
+            this.button_export.TabIndex = 3;
+            this.button_export.Text = "导出";
+            this.button_export.UseVisualStyleBackColor = true;
+            this.button_export.Click += new System.EventHandler(this.button_export_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 370);
+            this.Controls.Add(this.button_export);
+            this.Controls.Add(this.button_gen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_colcount);
+            this.Controls.Add(this.textBox_rowcount);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "yoyohate";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_rowcount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_colcount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_gen;
+        private System.Windows.Forms.Button button_export;
+    }
+}
+
---
+++ yoyohate/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace yoyohate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox_rowcount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsNumber(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_colcount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsNumber(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(e.RowBounds.Location.X,
+
+                e.RowBounds.Location.Y,
+
+                dataGridView1.RowHeadersWidth - 4,
+
+                e.RowBounds.Height);
+
+
+
+            TextRenderer.DrawText(e.Graphics, (e.RowIndex + 1).ToString(),
+
+                dataGridView1.RowHeadersDefaultCellStyle.Font,
+
+                rectangle,
+
+                dataGridView1.RowHeadersDefaultCellStyle.ForeColor,
+
+                TextFormatFlags.VerticalCenter | TextFormatFlags.Right);
+        }
+
+        private void button_gen_Click(object sender, EventArgs e)
+        {
+            int rowCount = int.Parse(textBox_rowcount.Text);
+            int colCount = int.Parse(textBox_colcount.Text);
+            if ((rowCount <= 0) || (colCount <= 0))
+            {
+                MessageBox.Show("你脑子秀逗了？");
+                return;
+            }
+            if ((rowCount > 15) || (colCount > 15))
+            {
+                MessageBox.Show("这绝对不是亲妈，想累死个人啊");
+            }
+            DataTable dt = new DataTable();
+            for (int i = 0; i < colCount; i++)
+            {
+                dt.Columns.Add((i + 1).ToString());
+            }
+            for (int i = 0; i < rowCount; i++)
+            {
+                DataRow r = dt.NewRow();
+                for (int j = 0; j < colCount; j++)
+                {
+                    r[j] = "";
+                }
+                dt.Rows.Add(r);
+            }
+            Random ran = new Random();
+            for (int i = 0; i < colCount * rowCount / 2; i++)
+            {
+                do
+                {
+                    int r = ran.Next(0, rowCount);
+                    int c = ran.Next(0, colCount);
+                    if (dt.Rows[r][c] as string == "")
+                    {
+                        dt.Rows[r][c] = i + 1;
+                        break;
+                    }
+                } while (true);
+            }
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button_export_Click(object sender, EventArgs e)
+        {
+            DataTable dt = dataGridView1.DataSource as DataTable;
+            if (dt == null)
+            {
+                MessageBox.Show("你脑子秀逗了？");
+                return;
+            }
+            string outputText = "";
+            foreach (DataRow r in dt.Rows)
+            {
+                for (int i = 0; i < r.ItemArray.Length; i++)
+                {
+                    outputText += r[i] as string + ",";
+                }
+                outputText = outputText.Substring(0, outputText.Length - 1) + "\r\n";
+            }
+            System.IO.FileStream fs = new System.IO.FileStream("./yoyohate.csv", System.IO.FileMode.Create, System.IO.FileAccess.Write);
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(fs);
+            fs.SetLength(0);
+            sw.Write(outputText);
+            sw.Close();
+            fs.Close();
+            MessageBox.Show("yoyo会恨你的，去打开yoyohate.csv吧");
+        }
+    }
+}
